e126ff948c46c071454fc8a0ba5839845b4b5b7c
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-06-28 00:32:14 +0800

    Added Base Renderer
    Output all matches, Rainbow everything with the triangle mask tactic

---
+++ CustomGameEngine/BaseRenderer/AppWindow.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+class AppWindow: public Window
+{
+public:
+	AppWindow();
+	~AppWindow();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+private:
+	SwapChain * m_swap_chain;
+	VertexBuffer* vb_RR;
+	VertexBuffer* vb_GR;
+	VertexBuffer* vb_RTC;
+	VertexBuffer* vb_RTLM;
+	VertexBuffer* vb_RTRM;
+	VertexShader* m_vs;
+	PixelShader* m_ps;
+};
+
