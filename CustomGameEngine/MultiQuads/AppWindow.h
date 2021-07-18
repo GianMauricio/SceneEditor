a49aaf3fb97051cae36d105b6c1623700436b80c
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-07-18 12:27:12 +0800

    AddingBaseProject

---
+++ CustomGameEngine/MultiQuads/AppWindow.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+class AppWindow: public Window
+{
+public:
+	AppWindow();
+	~AppWindow();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+private:
+	SwapChain * m_swap_chain;
+	VertexBuffer* m_vb;
+	VertexShader* m_vs;
+	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
+
+	unsigned long m_old_time = 0;
+	float m_delta_time = 0;
+	float m_angle = 0;
+};
+
